981b9f4a1ff16c79a390052c42542150e8023938
kamalgodar <kamalgodar9244@gmail.com>
2020-05-25 20:55:54 -1200

    refactored key layout

--- lib/main.dart
+++ lib/main.dart
@@ -26,11 +26,15 @@ class MyApp extends StatelessWidget {
    Widget buildbutton(String text){
         return Expanded(
                  child: MaterialButton(
-                   child: Text(text),
+                   child: Text(text,
+                            style:TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold )),
                    color: Colors.orangeAccent,
                    splashColor: Colors.white,
+                   shape: RoundedRectangleBorder(
+                     borderRadius: BorderRadius.circular(30),
+                     side: BorderSide(color: Colors.black)),
                    padding: EdgeInsets.symmetric(
-                     horizontal: 10,
+                     horizontal: 20,
                      vertical: 30),
                    onPressed: (){},
                  ),
@@ -39,42 +43,47 @@ class MyApp extends StatelessWidget {
    @override
    Widget build(BuildContext context) {
      return Scaffold(
+         backgroundColor: Colors.orange[200],
          appBar: AppBar(title: Text('Calculator'),
          backgroundColor: Colors.orangeAccent,
          ),
+        
          body: Container(
-           //padding: EdgeInsets.symmetric(vertical: 10),
+           padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
            //alignment: Alignment.bottomCenter,
            child: Column(
+             mainAxisSize: MainAxisSize.max,
              mainAxisAlignment: MainAxisAlignment.end,
              children: <Widget>[
              Row(children: <Widget>[
-               buildbutton("2"),
-               buildbutton("6"),
-               buildbutton("2"),
-               buildbutton("2"),
-               buildbutton("2"),
+               buildbutton("AC"),
+               buildbutton("DEL"),
+               buildbutton("%"),
+               buildbutton("/"),
              ]),
              Row(children: <Widget>[
-              buildbutton("2"),
-              buildbutton("2"),
-              buildbutton("2"),
-              buildbutton("2"),
-              buildbutton("2"),
+              buildbutton("7"),
+              buildbutton("8"),
+              buildbutton("9"),
+              buildbutton("x"),
              ]),
                Row(children: <Widget>[
-               buildbutton("2"),
+               buildbutton("4"),
+               buildbutton("5"),
                buildbutton("6"),
-               buildbutton("2"),
-               buildbutton("2"),
-               buildbutton("2"),
+               buildbutton("-"),
              ]),
                Row(children: <Widget>[
+               buildbutton("1"),
                buildbutton("2"),
-               buildbutton("6"),
-               buildbutton("2"),
-               buildbutton("2"),
-               buildbutton("2"),
+               buildbutton("3"),
+               buildbutton("+"),
+             ]),
+              Row(children: <Widget>[
+               buildbutton("."),
+               buildbutton("0"),
+               buildbutton("00"),
+               buildbutton("="),
              ]),
            ],)
          )
